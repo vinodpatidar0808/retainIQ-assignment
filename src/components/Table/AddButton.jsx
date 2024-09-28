a2d9f77f4f84038bbfe8fa214136c4213f924b43
Vinod Patidar <vinodpatidar0808@gmail.com>
2024-09-28 11:36:25 +0530

    Row hover delete button

--- src/components/Table/AddButton.jsx
+++ src/components/Table/AddButton.jsx
@@ -1,15 +1,11 @@
 import { FaPlus } from 'react-icons/fa6';
 
-const AddButton = ({ text = '' }) => {
+const AddButton = ({isProductFilter , text = '' }) => {
   return (
     <div
-      className={`${
-        text ? 'bg-white' : ''
-      } h-full border-2 border-dashed flex flex-col items-center px-6 py-3 text-xs gap-1 text-gray-700  justify-center `}>
+      className={`bg-white h-full border-2 border-dashed flex flex-col items-center px-6 py-3 text-xs gap-1 text-gray-700  justify-center `}>
       <button
-        className={`${
-          !text ? 'bg-white' : ''
-        } flex w-fit border py-3 px-3 rounded-md items-center gap-2  text-ellipsis overflow-hidden whitespace-nowrap`}>
+        className={`bg-white flex w-fit border py-3 px-3 rounded-md items-center gap-2  text-ellipsis overflow-hidden whitespace-nowrap `}>
         <FaPlus />
         {text}
       </button>
--- src/components/Table/Rownumber.jsx
+++ src/components/Table/Rownumber.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react';
+import { FaRegTrashCan } from 'react-icons/fa6';
 import { LuGrip } from 'react-icons/lu';
 
 const Rownumber = () => {
+  const [hover, setHover] = useState(false);
+  const handleRowDelete = () => {
+    console.log('delete row ');
+  };
+
   return (
-    <div className="min-w-32 px-3 flex items-center justify-end  text-black font-bold text-xl relative bg-red-500">
-      <p className="bg-blue-400">1</p>
+    <div
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className="min-w-32 px-3 flex items-center justify-end  text-black font-bold text-xl relative ">
+      {hover && (
+        <span
+          className="absolute top-11 right-4 cursor-pointer"
+          onClick={handleRowDelete}>
+          <FaRegTrashCan className="fill-red-500 " />
+        </span>
+      )}
+      <p className="">1</p>
       <LuGrip className="fill-black" />
     </div>
   );
